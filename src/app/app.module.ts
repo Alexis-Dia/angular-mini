96617e131d4bd344a024151f6322fc669ea8b144
Alexis-Dk <alexeydruzik@inbox.ru>
2017-11-06 16:59:28 +0300

    lesson 2 (reverted from commit 307b3b73d00cf5490dc9ff8dd156bce02b27b863)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {UserComponent} from "./user/user.component";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
