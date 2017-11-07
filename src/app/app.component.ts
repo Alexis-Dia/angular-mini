74660c5a13129c7329ddf9d79fc770c496617905
Alexis-Dk <alexeydruzik@inbox.ru>
2017-11-07 14:49:45 +0300

    Lesson 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
+import {UsersService} from "./users.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [UsersService]
 })
 export class AppComponent {
   // title = 'app';
+  users = [];
 
-  users = [
-    {name: 'WFM 1'},
-    {name: 'WFM 2'},
-    {name: 'WFM 3'},
-    {name: 'WFM 4'},
-    {name: 'WFM 5'},
-    {name: 'WFM 6'},]
+  constructor(private usersService: UsersService) {
+    //this.users = this.usersService.users
+  }
 
+  /*Best practise insted constructor in constructor use this component*/
+  ngOnInit () {
+    /*this.users = this.usersService.users*/
+    this.usersService.getUsers().subscribe(users => {
+      /*console.log(users);*/
+      this.users = users;
+    });
+  }
 
 }
