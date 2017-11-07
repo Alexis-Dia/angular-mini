fbb9fa4b0b36c015ec7c2d4df1d3c4fef9d2ce40
Alexis-Dk <alexeydruzik@inbox.ru>
2017-11-07 10:21:29 +0300

    Lesson 3 changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+
+  users = [
+    {name: 'WFM 1'},
+    {name: 'WFM 2'},
+    {name: 'WFM 3'},
+    {name: 'WFM 4'},
+    {name: 'WFM 5'},
+    {name: 'WFM 6'},]
+
+
 }
